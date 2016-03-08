c786fc01bcbba3e61afda30b14524c1bd0f592cb
Rodrigo Fernandes <cyber_fusion@gmx.net>
2016-03-08 20:16:30 +0000

    Improve Pid functions

--- command/Pid.cpp
+++ command/Pid.cpp
@@ -1,61 +1,132 @@
 #include "Pid.h"
 
-#include <signal.h>
+#include <fstream>
 
 #include <experimental/filesystem>
 
 #include <boost/spirit/home/x3.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 
+#include <signal.h>
+#include <sys/wait.h>
+
 using namespace std;
 using namespace std::experimental::filesystem;
 
-namespace ast {
+namespace {
+  // TODO: move to Utils header
+  string slurp(const istream& in){
+    return static_cast<stringstream const&>(stringstream() << in.rdbuf()).str();
+  }
+}
 
+namespace ast {
   using Pid = boost::variant<unsigned, string>;
+
+  struct kill_function{
+    boost::optional<uint8_t> signal;
+  };
+
+  using wait_function = uint64_t;
+
+  using cwd_function = string;
+
+  using functions = boost::variant<kill_function, wait_function, cwd_function>;
 
   struct pid_command{
     boost::optional<Pid> pid;
-    boost::optional<string> function;
+    boost::optional<functions> function;
   };
 }
 
 BOOST_FUSION_ADAPT_STRUCT(ast::pid_command, pid, function)
+BOOST_FUSION_ADAPT_STRUCT(ast::kill_function, signal)
 
 namespace {
   namespace x3 = boost::spirit::x3;
 
-  auto functions = x3::rule<class functions, std::string>()
-    = x3::string("kill") |
-      x3::string("wait") |
-      x3::string("cwd");
+  auto killFunction = x3::rule<class killFunction, ast::kill_function>()
+    = x3::lit("kill") >> -( x3::lit('(') >> x3::uint8 >> x3::lit(')') );
 
-  template<typename T>
-    auto as = [](auto p) { return x3::rule<struct _, T>{} = x3::as_parser(p); };
+  auto waitFunction = x3::rule<class waitFunction, ast::wait_function>()
+    = x3::lit("wait") >> x3::lit('(') >> x3::uint64 >> x3::lit(')');
+
+  // TODO: There is probably a cleaner approach to bind a type with a literal
+  //  that would remove the need for the cwd_function using declaration
+  auto cwdFunction =  x3::rule<class cwdFunction, ast::cwd_function>()
+    = x3::string("cwd");
+
+  auto functions = x3::rule<class functions, ast::functions>()
+    = killFunction | waitFunction | cwdFunction;
+
+  auto pidName = x3::rule<class pidName, string>()
+    = x3::lit('"') >> x3::no_skip[+~x3::char_('"')] >> x3::lit('"') |
+      +~x3::char_(' ');
 
   auto pidCommand = x3::rule<class pidCommand, ast::pid_command>()
-    = '^' >> -x3::uint_ >> -functions;
+    = '^' >> - (x3::uint_ | pidName) >> - x3::no_skip[ x3::lit('.') >> functions ];
 
-
-  struct pidVisitor {
+  struct PidVisitor {
     unsigned operator()(unsigned& i) const {
       return i;
     }
 
-    unsigned operator()(std::string& /*str*/) const {
-      // TODO: get pid by name, return list of pids instead
-      return 31337;
+    unsigned operator()(std::string& str) const {
+      for(auto& entry: directory_iterator{"/proc"}) {
+        if (is_directory(entry) && !is_symlink(entry)) {
+          ifstream fin{(entry.path() / "cmdline").string()};
+          if (!fin){
+            continue;
+          }
+          auto cmdline = slurp(fin);
+          cmdline = cmdline.substr(0, cmdline.find_first_of('\0'));
+          if (cmdline.find(str) != string::npos){
+            return stoul(entry.path().stem());
+          }
+        }
+      }
+      // FIXME: when lists are implemented return all pids instead of just the first
+      return 0;
     }
   };
 
+  class FunctionVisitor {
+    unsigned _pid;
+    Output& _out;
+
+  public:
+    FunctionVisitor(unsigned pid, Output& output)
+    : _pid{pid}
+    , _out{output}
+    {
+    }
+
+    void operator()(const ast::kill_function& function) const {
+      auto signal = function.signal.get_value_or(6);
+      kill(_pid, signal);
+      _out << "killed [" << signal << "]: " << _pid << "\n";
+    }
+
+    void operator()(const ast::wait_function& /*function*/) const {
+      _out << "waiting : " << _pid << "\n";
+      wait(const_cast<unsigned*>(&_pid));
+    }
+
+    void operator()(const ast::cwd_function&) const {
+      path pidCwd{"/proc"};
+      pidCwd /= to_string(_pid) / "cwd";
+      // TODO: Read_symlink isn't working with /proc symlink ??
+      _out << read_symlink(pidCwd).string() << "\n";
+    }
+  };
 }
 
 bool Pid::matches(const Line & line) const {
-  string command = line();
+  const string command {line()};
   auto iter = command.begin();
   auto endIter = command.end();
 
-  bool ok {x3::phrase_parse(iter, endIter, pidCommand, x3::space)};
+  const bool ok {x3::phrase_parse(iter, endIter, pidCommand, x3::space)};
 
   return ok || static_cast<size_t>(distance(command.begin(), iter)) == command.size();
 }
@@ -64,36 +135,23 @@ Command::Suggestions Pid::suggestions(const Line & /*line*/) const {
   return {};
 }
 
-Command::Status Pid::execute(const Line & line, Output & curses){
-  string command = line();
-
+Command::Status Pid::execute(const Line & line, Output & output){
+  const string command {line()};
   auto iter = command.begin();
   auto endIter = command.end();
 
   ast::pid_command data;
-  bool ok {x3::phrase_parse(iter, endIter, pidCommand, x3::space, data)};
+  const bool ok {x3::phrase_parse(iter, endIter, pidCommand, x3::space, data)};
 
   if (!ok) return Command::Status::NoMatch;
 
-  unsigned pid = data.pid ? boost::apply_visitor(pidVisitor{}, data.pid.get())
-                          : getpid();
+  const unsigned pid = data.pid ? boost::apply_visitor(PidVisitor{}, data.pid.get())
+                                : getpid();
 
   if (data.function) {
-    const string& function = data.function.get();
-
-    // TODO: Map this to enum
-    if (function == "kill") {
-      kill(pid, 15);
-      curses << "killed [15]: " << pid << "\n";
-    } else if (function == "cwd" ){
-      path pidCwd{"/proc"};
-      pidCwd /= to_string(pid) / "cwd";
-      curses << read_symlink(pidCwd).string() << "\n";
-    } else {
-      curses << data.function.get() << " not implemented" << "\n";
-    }
-  } else { // Just print pid << "\n";
-    curses << pid;
+    boost::apply_visitor(FunctionVisitor{pid, output}, data.function.get());
+  } else {
+    output << pid << "\n";
   }
   return Command::Status::Ok;
 }
